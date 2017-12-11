aa2f4d04fa7f4c48f3da7dc7ad9f27baeb17dfd0
Leonardo Wellausen <lwellausen@gmail.com>
2017-12-11 21:47:03 -0200

    fixes

--- trab.ml
+++ trab.ml
@@ -29,7 +29,10 @@ type term = TmN of int
 	| TmLet_rec of ident * (typ * typ) * (ident * typ * term) * term;;
 	
 
-	
+(*exceptions*)
+exception Identifier_Not_Found
+exception Now_its_Exhaustive
+
 (*Big Step Eval :*)	
 type value = N of int
 	| B of bool
@@ -73,29 +76,35 @@ let rec bs_eval environment e : value = match e with
 	(*BS-LET*)
 	|TmLet(x, this_type, e1, e2) ->
 		let this_term = bs_eval environment e1 in
-		(eval (insertEnv x this_term environment) e2)
+		(bs_eval (insertEnv x this_term environment) e2)
 	
 	(*BS-LETREC*)
 	|TmLet_rec(f, (typeI, typeO), (x, typeX, e1), e2) ->
 		let this_term = bs_eval environment (TmFn(x, typeX, e1)) in
 		(match this_term with
-			| Clsr(x_term, func_term, env_term) -> bs_eval (insertEnv f (Clsr(f, x_term, func_term, environment)) env_term) e2 
+			| Clsr(x_term, func_term, env_term) -> bs_eval (insertEnv f (RecClsr(f, x_term, func_term, environment)) env_term) e2 
+			| _ -> raise Now_its_Exhaustive (*killing warnings*)
 		)
 
 	(*BS-APP*)
+	(*
 	| TmEE(e1, e2) ->
 		let term_e1 = bs_eval environment e1 in
 		let term_e2 = bs_eval environment e2 in
 		(match term_e1, term_e2 with
 			Clsr(x, e1_term, e1_env), val_e2 -> bs_eval (insertEnv x val_e2 e1_env) e1_term
+			| _ -> raise Now_its_Exhaustive (*killing warnings*)
 		)
+	*)
 
-	(*BS-APPREC*)
+	(*BS_APP +++ BS-APPREC  juntas porque usam o mesmo pattern matching*)
 	| TmEE(e1, e2) ->
 		let term_e1 = bs_eval environment e1 in
 		let term_e2 = bs_eval environment e2 in
 		(match term_e1, term_e2 with
-			RecClsr(f, x, e1_term, e1_env), val_e2 -> bs_eval (insertEnv f (Clsr(f, x, e1_term, e1_env)) (insertEnv x val_e2 e1_env)) e1_term
+			Clsr(x, e1_term, e1_env), val_e2 -> bs_eval (insertEnv x val_e2 e1_env) e1_term
+			|RecClsr(f, x, e1_term, e1_env), val_e2 -> bs_eval (insertEnv f (RecClsr(f, x, e1_term, e1_env)) (insertEnv x val_e2 e1_env)) e1_term
+			| _ -> raise Now_its_Exhaustive (*killing warnings*)
 		)
 
 	(*BS-OP    não está no resumo-l1 mas parece necessário*)
@@ -104,19 +113,25 @@ let rec bs_eval environment e : value = match e with
 		let term_e2 = bs_eval environment e2 in
 		(match term_e1, op, term_e2 with
 			N(v1), Sum, N(v2) -> N(v1 + v2)
-			N(v1), Sub, N(v2) -> N(v1 - v2)
-			N(v1), Mult, N(v2) -> N(v1 * v2)
-			N(v1), Great, N(v2) -> B(v1 > v2)
-			N(v1), GreatEq, N(v2) -> B(v1 >= v2)
-			N(v1), Eq, N(v2) -> B(v1 = v2)
-			N(v1), Diff, N(v2) -> B(v1 <> v2)
-			N(v1), LessEq, N(v2) -> B(v1 <= v2)
-			N(v1), Less, N(v2) -> B(v1 < v2)
-		);;
+			|N(v1), Sub, N(v2) -> N(v1 - v2)
+			|N(v1), Mult, N(v2) -> N(v1 * v2)
+			|N(v1), Great, N(v2) -> B(v1 > v2)
+			|N(v1), GreatEq, N(v2) -> B(v1 >= v2)
+			|N(v1), Eq, N(v2) -> B(v1 = v2)
+			|N(v1), Diff, N(v2) -> B(v1 <> v2)
+			|N(v1), LessEq, N(v2) -> B(v1 <= v2)
+			|N(v1), Less, N(v2) -> B(v1 < v2)
+			
+			| _ -> raise Now_its_Exhaustive (*killing warnings*)
+		)
+		
+	| _ -> raise Now_its_Exhaustive (*killing warnings*);;
 
 
 (*type system*)
-type 
+type type_env = (ident * typ) list;;
+
+
 	
 	
 
